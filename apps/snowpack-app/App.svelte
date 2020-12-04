be76c67783b144d9c7445bb05f2133a8564ab2ff
Antony Jones <antony@beyonk.com>
2020-12-04 11:54:12 +0000

    fix missing app

---
+++ apps/snowpack-app/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import A from 'a'
+  // import B from 'b'
+  import C from './C.svelte'
+</script>
+
+<div>
+  <p>Hello</p>
+
+  <A />
+  <C />
+</div>
---
+++ apps/snowpack-app/snowpack.config.js
@@ -0,0 +1,27 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    'packageLookupFields': [ 'svelte' ]
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
